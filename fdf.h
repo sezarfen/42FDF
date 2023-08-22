e05fd3f568da1cadab7b751916c87db3f42267d5
Sezarfen <110563468+sezarfen@users.noreply.github.com>
2023-08-22 22:45:10 +0300

    Update fdf.h

--- fdf.h
+++ fdf.h
@@ -40,7 +40,7 @@ void	free_split(char **split);
 t_data	*read_map(char *file_name);
 int		get_map_height(char *file_name);
 int		get_map_width(char *file_name);
-void	set_map(t_data *data, char *file_name, int i, int fd);
+void	set_map(t_data *data, int i, int fd);
 int 	max(int a, int b);
 void	connect_indexes(t_data *data);
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color);
